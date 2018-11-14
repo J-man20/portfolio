e92e77f9bfba9a49731e8d79bedcfaf75633c882
J-man20 <prcrown4life@gmail.com>
2018-11-14 11:11:10 -0500

    Starting to work on slick

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -1,22 +1,5 @@
 $(document).ready(function(){
   $('.slideshow').slick({
     slidesToShow:1,
-    responsive: [
-    {
-      breakpoint: 1109,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 710,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    ]
-  }),
+  });
 });
