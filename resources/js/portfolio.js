6dc909963bff238493dea702bc9d4807be262f02
J-man20 <prcrown4life@gmail.com>
2018-11-14 11:23:32 -0500

    Made slick responsive

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
   $('.slideshow').slick({
     slidesToShow:1,
+    adaptiveHeight:true,
+    adaptiveWidth:true,
+    responsive: [
+    {
+      breakpoint: 1109,
+      settings: {
+        slidesToShow: 1,
+
+      }
+    },
+    {
+      breakpoint: 710,
+      settings: {
+        slidesToShow: 1,
+
+      }
+    },
+  ]
   });
 });
